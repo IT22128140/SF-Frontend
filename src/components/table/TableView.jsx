e81beb79cdd4307c7f8a224c2c282abbffd94785
IT22099990 <it22099990@my.sliit.lk>
2024-03-30 09:38:56 +0530

    0330new#3

--- src/components/table/TableView.jsx
+++ src/components/table/TableView.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const TableView = ({ headers }) => {
   return (
     <thead>
-      <tr>
+      <tr className='bg-orange-100'>
         {headers.map((header, index) => (
-          <th className='border border-slate-600 rounded-md max-md:hidden' key={index}>{header}</th>
+          <th className='border border-slate-700 px-6 py-3 text-center text-sm font-medium uppercase tracking-wider' key={index}>{header}</th>
         ))}
       </tr>
     </thead>
