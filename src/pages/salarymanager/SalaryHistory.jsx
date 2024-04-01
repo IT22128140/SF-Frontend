a17a22e7e01bb0f29c3d3d8402c5fd21270c9368
it22196774 <it22196774@my.sliit.lk>
2024-04-01 12:25:37 +0530

    Gihan3

--- src/pages/salarymanager/SalaryHistory.jsx
+++ src/pages/salarymanager/SalaryHistory.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { set } from 'react-hook-form';
 import TableView from '../../components/table/TableView';
 import Spinner from '../../components/Spinner';
+import SearchBar from '../../components/SearchBar';
 
 
 
@@ -11,14 +12,15 @@ import Spinner from '../../components/Spinner';
 const SalaryHistory = () => {
   const [salaryHistory, setSalaryHistory] = useState([]);
   const [loading, setLoading] = useState(false);
-  const headers = ['EmployeeName','time', 'fullname', 'basicSalary','attendance',' overtime','bonus','totalAmount','notice','cheques','profile' ];
+  const headers = ['EmployeeID','Employee Name','Operation','Time'];
 
   useEffect(() => {
     setLoading(true);
     axios
-      .get('http://localhost:5000/salary')
+      .get('http://localhost:5555/salary')
       .then((response) => {
-        setSalaryHistory(response.data.data);
+        console.log(response.data);
+        setSalaryHistory(response.data);
         setLoading(false);
       })
       .catch((error) => {
@@ -26,33 +28,35 @@ const SalaryHistory = () => {
         setLoading(false);
       });
   }, []);
+  const handleView = () =>{
+  
+  };
+
   return (
     <div className='p-4'>
       <div className='flex justify-between items-center'>
         <h1 className='text-3xl my-8'>Salary List</h1>
       </div>
+      <br>
+      </br>
+      <br>
+      </br>
+      <SearchBar placeholder={"Enter Employee Name OR Employee ID"} />
       {loading ? (
         <Spinner />
       ) :(
         <table className='min-w-full'>
           <TableView headers={headers} />
           <tbody>
-            {salaryHistory.map((salary,index) => {
+            {salaryHistory.map((salary,index) => (
               <tr key={salary._id} className='h-8'>
+                <td className='border border-slate-700 rounded-md text-center'>{salary._id}</td>
                 <td className='border border-slate-700 rounded-md text-center'>{salary.name}</td>
-                <td className='border border-slate-700 rounded-md text-center'>{salary.time}</td>
                 <td className='border border-slate-700 rounded-md text-center'>{salary.fullName}</td>
-                <td className='border border-slate-700 rounded-md text-center'>{salary.basicSalary}</td>
-                <td className='border border-slate-700 rounded-md text-center'>{salary.attendance}</td>
-                <td className='border border-slate-700 rounded-md text-center'>{salary.overtime}</td>
-                <td className='border border-slate-700 rounded-md text-center'>{salary.bonus}</td>
-                <td className='border border-slate-700 rounded-md text-center'>{salary.totalAmount}</td>
-                <td className='border border-slate-700 rounded-md text-center'>{salary.notice}</td>
-                <td className='border border-slate-700 rounded-md text-center'>{salary.cheques}</td>
-                <td className='border border-slate-700 rounded-md text-center'>{salary.profile}</td>
-               
-               </tr>
-               })}
+                <td className='border border-slate-700 rounded-md text-center'>{salary.time}</td>
+                
+              </tr>
+               ))}
 
           </tbody>
         </table>
