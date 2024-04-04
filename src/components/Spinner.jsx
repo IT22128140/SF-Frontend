ee5d8ca9665bf40065deef3eeeb6c7ed33bd6a10
IT22128140 <it22128140@my.sliit.lk>
2024-04-04 22:36:45 +0530

    all

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
 
 const Spinner = () => {
   return (
-    <div className= 'animate-ping w-16 h-16 m-8 rounded-full bg-orange-600'></div>
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-primary"></div>
+    </div>
+    // <div className='animate-ping w-16 h-16 m-8 rounded-full bg-orange-600'></div>
   )
 }
 
