06c9b247dea15f9f1e740a612918b60590718a66
IT22094704 <it22094704@my.sliit.lk>
2024-04-09 10:03:28 +0530

    homeContain

--- src/components/homecontainer/CustomCard1.jsx
+++ src/components/homecontainer/CustomCard1.jsx
@@ -5,8 +5,8 @@ import { motion } from "framer-motion";
 const CustomCard1 = ({ header, description, buttonLabel, buttonClassName, buttonOnClick, className }) => {
   return (
     <div className={`m-12 bg-bgc h-60 w-96 md:w-1/4 rounded-xl hover:bg-bgc hover:scale-110 duration-700 p-5 ${className}`}>
-      <h2 className="py-3 text-2xl text-white font-BreeSerif text-center">{header}</h2>
-      <p className="text-base leading-7 text-white font-Philosopher space-y-4 text-center">{description}</p>
+      <h2 className="py-3 text-2xl text-ternary font-BreeSerif text-center">{header}</h2>
+      <p className="text-base leading-7 text-secondary font-Philosopher space-y-4 text-center">{description}</p>
       <div className="pt-7 pb-2 flex justify-center">
         <motion.button 
           className={`w-36 h-10 font-BreeSerif rounded-md bg-primary hover:scale-90 duration-500 ${buttonClassName}`} 
