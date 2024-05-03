e12d6d1924b993b712353a494b5fb248f8668bfd
IT22289216 <it22289216@my.sliit.lk>
2024-05-03 16:06:31 +0530

    hi

--- src/pages/RegisCus.jsx
+++ src/pages/RegisCus.jsx
@@ -1,141 +1,178 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from 'axios';
 import NavbarLogo from "../components/navbar/NavbarLogo";
 import Footer from "../components/footer/Footer";
-import { Link } from "react-router-dom";
 
-class Register extends Component {
-  constructor() {
-    super();
-    this.state = {
-      FirstName: "",
-      LaststName: "",
-      emailAddress: "",
-      phoneNumber: "",
-      password: "",
-      password2: "",
-      errors: {}
-    };
-  }
+function Register() {
+  const [FirstName, setFirstName] = useState("");
+  const [LastName, setLastName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-  onChange = e => {
-    this.setState({ [e.target.id]: e.target.value });
+  const onChange = (e) => {
+    const { id, value } = e.target;
+    switch (id) {
+      case "FirstName":
+        setFirstName(value);
+        break;
+      case "LastName":
+        setLastName(value);
+        break;
+      case "emailAddress":
+        setEmailAddress(value);
+        break;
+      case "phoneNumber":
+        setPhoneNumber(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      case "password2":
+        setPassword2(value);
+        break;
+      default:
+        break;
+    }
   };
 
-  onSubmit = e => {
+  const onSubmit = (e) => {
     e.preventDefault();
 
+    if (!FirstName || !LastName || !emailAddress || !phoneNumber || !password || !password2) {
+      setError("Please fill in all the fields.");
+      return;
+    }
+
+    if (!/\S+@\S+\.\S+/.test(emailAddress)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+    if (password !== password2) {
+      setError("Passwords do not match.");
+      return;
+    }
+
     const newUser = {
-      FirstName: this.state.FirstName,
-      LaststName: this.state.LaststName,
-      emailAddress: this.state.emailAddress,
-      phoneNumber: this.state.phoneNumber,
-      password: this.state.password,
-      password2: this.state.password2
+      FirstName,
+      LastName,
+      emailAddress,
+      phoneNumber,
+      password
     };
 
     console.log(newUser);
+
+    axios.post("http://localhost:3001/register", newUser)
+      .then((result) => {
+        console.log(result);
+        navigate("/LoginCus");
+      })
+      .catch((err) => console.log(err));
   };
 
-  render() {
-    const { errors } = this.state;
 
-    return (
-      <div className="flex flex-col items-center select-none">
-        <NavbarLogo />
-        <div className="flex justify-center">
-          <div className="w-full max-w-md">
-            <div className="text-center mt-">
-              <h4 className="text-3xl font-bold mb-4">CUSTOMER REGISTRATION FORM</h4>
-            </div>
-            <form noValidate onSubmit={this.onSubmit}>
-              <div className="mb-4">
-                <p className="text-black-600 mb-4">First Name </p>
-                <input
-                  onChange={this.onChange}
-                  value={this.state.FirstName}
-                  error={errors.FirstName}
-                  id="FirstName"
-                  type="text"
-                  placeholder="First Name"
-                  className="mt-0 w-[100%] p-3 border-gray-300 rounded-md border-2"
-                />
-              </div>
-              <div className="mb-4">
-                <p className="text-black-600 mb-4">Last Name </p>
-                <input
-                  onChange={this.onChange}
-                  value={this.state.LastName}
-                  error={errors.name}
-                  id="LastName"
-                  type="text"
-                  placeholder="Last Name"
-                  className="mt-0 w-[100%] p-3 border-gray-300 rounded-md border-2"
-                />
-              </div>
-              <div className="mb-4">
-                <p className="text-black-600 mb-4">Email Address </p>
-                <input
-                  onChange={this.onChange}
-                  value={this.state.emailAddress}
-                  error={errors.emailAddress}
-                  id="emailAddress"
-                  type="email"
-                  placeholder="Email Address"
-                  className="mt-0 w-[100%] p-3 border-gray-300 rounded-md border-2"
-                />
-              </div>
-              <div className="mb-4">
-                <p className="text-black-600 mb-4">Phone Number </p>
-                <input
-                  onChange={this.onChange}
-                  value={this.state.phoneNumber}
-                  error={errors.phoneNumber}
-                  id="phoneNumber"
-                  type="phone Number"
-                  placeholder="Phone Number"
-                  className="mt-0 w-[100%] p-3 border-gray-300 rounded-md border-2"
-                />
-              </div>
-              <div className="mb-4">
-                <p className="text-black-600 mb-4">Password </p>
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password}
-                  error={errors.password}
-                  id="password"
-                  type="password"
-                  placeholder="Password"
-                  className="mt-0 w-[100%] p-3 border-gray-300 rounded-md border-2"
-                />
-              </div>
-              <div className="mb-4">
-                <p className="text-black-600 mb-4">Re-Type Password</p>
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password2}
-                  error={errors.password2}
-                  id="password2"
-                  type="password"
-                  placeholder="Re-Type Password"
-                  className="mt-0 w-[100%] p-3 border-gray-300 rounded-md border-2"
-                />
-              </div>
-              <div className="text-center">
-                <button
-                  type="submit"
-                  className="mt-3 w-[100%] p-3 bg-orange-600 text-white rounded-md"
-                >
-                  REGISTER
-                </button>
-                <p className="text-gray-600 mb-4">Already have an account? <Link to="/LoginCus" className="text-blue-500">Log in</Link></p>
-              </div>
-            </form>
-          </div>
+  return (
+  <div className="flex flex-col items-center select-none">
+    <NavbarLogo />
+    <div className="flex justify-center">
+      <div className="w-full max-w-md">
+        <div className="text-center mt-">
+          <h4 className="text-3xl font-bold mb-4">CUSTOMER REGISTRATION FORM</h4>
         </div>
-        <Footer />
+        <form noValidate onSubmit={onSubmit}>
+          <div className="mb-4">
+            <p className="text-black-600 mb-4">First Name </p>
+            <input
+              onChange={onChange}
+              value={FirstName}
+              id="FirstName"
+              type="text"
+              placeholder="First Name"
+              className="mt-0 w-[100%] p-3 border-gray-300 rounded-md border-2"
+            />
+          </div>
+          <div className="mb-4">
+            <p className="text-black-600 mb-4">Last Name </p>
+            <input
+              onChange={onChange}
+              value={LastName}
+              id="LastName"
+              type="text"
+              placeholder="Last Name"
+              className="mt-0 w-[100%] p-3 border-gray-300 rounded-md border-2"
+            />
+          </div>
+          <div className="mb-4">
+            <p className="text-black-600 mb-4">Email Address </p>
+            <input
+              onChange={onChange}
+              value={emailAddress}
+              id="emailAddress"
+              type="email"
+              placeholder="Email Address"
+              className="mt-0 w-[100%] p-3 border-gray-300 rounded-md border-2"
+            />
+          </div>
+          <div className="mb-4">
+            <p className="text-black-600 mb-4">Phone Number </p>
+            <input
+              onChange={onChange}
+              value={phoneNumber}
+              id="phoneNumber"
+              type="text"
+              placeholder="Phone Number"
+              className="mt-0 w-[100%] p-3 border-gray-300 rounded-md border-2"
+            />
+          </div>
+          <div className="mb-4">
+            <p className="text-black-600 mb-4">Password </p>
+            <input
+              onChange={onChange}
+              value={password}
+              id="password"
+              type="password"
+              placeholder="Password"
+              className="mt-0 w-[100%] p-3 border-gray-300 rounded-md border-2"
+            />
+          </div>
+          <div className="mb-4">
+            <p className="text-black-600 mb-4">Re-Type Password</p>
+            <input
+              onChange={onChange}
+              value={password2}
+              id="password2"
+              type="password"
+              placeholder="Re-Type Password"
+              className="mt-0 w-[100%] p-3 border-gray-300 rounded-md border-2"
+            />
+          </div>
+          {error && <p className="text-red-500">{error}</p>}
+          <div className="text-center">
+            <button
+              type="submit"
+              className="mt-3 w-[100%] p-3 bg-orange-600 text-white rounded-md"
+            >
+              REGISTER
+            </button>
+            <p className="text-gray-600 mb-4">
+              Already have an account?{" "}
+              <Link to="/LoginCus" className="text-blue-500">
+                Log in
+              </Link>
+            </p>
+          </div>
+        </form>
       </div>
+    </div>
+    <Footer />
+  </div>
     );
   }
-}
 
-export default Register;
+
+export default Register;
