e074a270db23214cf5c2a74cf093f5712df69918
IT22094704 <it22094704@my.sliit.lk>
2024-05-13 19:21:36 +0530

    df

--- src/pages/QualityControl/PendingReview.jsx
+++ src/pages/QualityControl/PendingReview.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
-import SearchBar from "../../components/SearchBar3";
+import SearchBar from "../../components/SearchQE.jsx";
 import QENavbar from "../../components/navbar/staffheader/QENavbar";
 import TableView from '../../components/table/TableView'
 import Button from "../../components/button/Button";
